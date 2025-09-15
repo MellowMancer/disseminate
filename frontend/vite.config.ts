a2b5180f9974e48315a148048e026c6a89917892
MellowMancer <wyatharth@gmail.com>
2025-09-15 17:13:01 +0530

    tailwind support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,           // Listen on all network interfaces
     watch: {
       usePolling: true,   // Use polling for file watching inside Docker
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
