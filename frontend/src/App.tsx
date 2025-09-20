80d4255d72b957aba42aa1e3f618904854e1cd8a
MellowMancer <wyatharth@gmail.com>
2025-09-20 20:16:42 +0530

    removed obsolete routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import HomePage from "./pages/HomePage";
-import LocalAccountPage from "./pages/LocalAuth";
-import CloudAuthPage from "./pages/CloudAuthPage";
 import { SchedulerPage } from "./pages/SchedulerPage";
 import "./App.css";
 
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={ <HomePage />} />
         <Route path="/schedule" element={<SchedulerPage />} />
-        <Route path="/auth" element={<CloudAuthPage />} />
-        <Route path="/local" element={<LocalAccountPage />} />
       </Routes>
   );
 }
