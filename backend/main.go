a5695b065877af17eb41a5bcb12db3f113e46c33
MellowMancer <wyatharth@gmail.com>
2025-09-22 03:26:04 +0530

    supa

--- backend/main.go
+++ backend/main.go
@@ -4,54 +4,41 @@ import (
 	_ "database/sql"
 	_ "fmt"
 	_ "net/http"
-	_ "time"
 	"os"
+	_ "time"
 
+	"backend/handlers"
 	"backend/routes"
-	"backend/models"
 	"backend/services"
+	_ "github.com/dgrijalva/jwt-go"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo/v4"
-    _ "github.com/labstack/echo/v4/middleware"
-    _ "github.com/dgrijalva/jwt-go"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/labstack/echo/v4/middleware"
 )
-
-var (
-    db *gorm.DB
-)
-
-func init() {
-    var err error
-    dsn := "postgresql://user:password@localhost/database_name?sslmode=disable" // Update with your database credentials
-    db, err = gorm.Open("postgres", dsn)
-    if err != nil {
-        panic("failed to connect to database")
-    }
-
-    // Auto migrate the User model
-    db.AutoMigrate(&models.User{})
-}
 
 func main() {
 	supabaseURL := os.Getenv("SUPABASE_URL")
-    supabaseKey := os.Getenv("SUPABASE_KEY")
+	supabaseKey := os.Getenv("SUPABASE_SECRET_KEY")
+	jwtSecret := os.Getenv("SUPABASE_JWT_SECRET")
 
-	userService := services.NewUserService(supabaseURL, supabaseKey)
-
+	userService := services.NewUserService(supabaseURL, supabaseKey, jwtSecret)
+	userHandler := handlers.NewHandler(userService)
 
 	e := echo.New()
 
-	e.Static("/assets", ".../frontend/dist/assets")
-	e.Static("/favicon.ico", ".../frontend/dist/favicon.ico")
-	e.Static("/vite.svg", ".../frontend/dist/vite.svg")
-
-	e.File("/", ".../frontend/dist/index.html")
+	e.Static("/assets", "../frontend/dist/assets")
+	e.Static("/favicon.ico", "../frontend/dist/favicon.ico")
+	e.Static("/vite.svg", "../frontend/dist/vite.svg")
 
 	// Register auth routes on /auth prefix
 	authGroup := e.Group("/auth")
 	routes.RegisterAuthRoutes(authGroup, userService)
-	routes.RegisterAPIRoutes(e)
+	routes.RegisterAPIRoutes(e, userHandler)
+
+	e.File("/", "../frontend/dist/index.html")
+	e.GET("/*", func(c echo.Context) error {
+		return c.File("../frontend/dist/index.html")
+	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
