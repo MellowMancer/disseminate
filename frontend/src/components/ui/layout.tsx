f407dcbcce1bf73c68eafbee5ef9fd5b210d25b0
MellowMancer <wyatharth@gmail.com>
2025-09-24 03:58:57 +0530

    [feat]: added navigation bar

--- frontend/src/components/ui/layout.tsx
+++ frontend/src/components/ui/layout.tsx
@@ -1,44 +1,16 @@
 import React from "react";
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import { Button } from "@/components/ui/button";
-import { LogOut, LogInIcon } from "lucide-react";
+import { Outlet } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
+import { Navbar } from "./navbar";
 
 const Layout: React.FC = () => {
-  const { authenticated, setAuthenticated } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleLogout = () => {
-    fetch("/auth/logout", { method: "POST", credentials: "include" }).then(() => {
-      setAuthenticated(false);
-      navigate("/login");
-    });
-  };
-
-  const handleLoginButton = () => {
-    navigate("/login");
-  }
+  const { authenticated } = useAuth();
 
   return (
-    <div className="h-screen w-screen overflow-auto bg-background py-16 px-8 md:py-8 md:px-24 mx-auto ">
-      {authenticated && (
-        <div className="fixed top-3 left-3 z-1000">
-          <Button variant="default" size="lg" onClick={handleLogout} aria-label="Logout">
-            <LogOut className="mr-2 h-4 w-4" />
-            Logout
-          </Button>
-        </div>
-      )}
-      {!authenticated && location.pathname != "/login" && location.pathname != "/signup" && (
-        <div className="fixed top-3 left-3 z-1000">
-          <Button variant="default" size="lg" onClick={handleLoginButton} aria-label="toLogin">
-            <LogInIcon className="mr-2 h-4 w-4" />
-            Login
-          </Button>
-        </div>
-      )}
+    <div className="h-screen w-screen overflow-auto bg-background pt-28 py-16 px-8 md:py-8 md:px-24 mx-auto ">
+      <Navbar />
       <Outlet context={{ authenticated }} />
+
     </div>
   );
 };
---
+++ frontend/src/components/ui/navbar.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+    NavigationMenuIndicator
+} from "@/components/ui/navigationMenu"
+
+export function Navbar() {
+    const { authenticated, setAuthenticated } = useAuth();
+    const navigate = useNavigate();
+
+
+    const handleLogout = () => {
+        fetch("/auth/logout", { method: "POST", credentials: "include" }).then(() => {
+            setAuthenticated(false);
+            navigate("/login");
+        });
+    };
+
+    const handleLoginButton = () => {
+        navigate("/login");
+    }
+
+    const handleProfileButton = () => {
+
+    }
+
+    const handleHomeButton = () => {
+        navigate("/");
+    }
+
+    const handleAboutButton = () => {
+
+    }
+
+    return (
+        <div className="flex items-center justify-center">
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <div onClick={handleHomeButton} aria-label="Home">
+                            Home
+                        </div>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <div onClick={handleAboutButton} aria-label="About">
+                            About
+                        </div>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <div onClick={handleProfileButton} aria-label="Profile">
+                            Profile & Keys
+                        </div>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        {authenticated ? (<div onClick={handleLogout} aria-label="Logout">
+                            Logout
+                        </div>) : (<div onClick={handleLoginButton} aria-label="Login">
+                            Login
+                        </div>)
+                        }
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuIndicator className="NavigationMenuIndicator" />
+            </NavigationMenuList>
+        </NavigationMenu>
+        </div>
+    )
+}
