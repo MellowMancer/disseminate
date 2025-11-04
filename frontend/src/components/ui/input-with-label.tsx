c4718045a763060dbd5bc4ac48aeab33cd92b670
MellowMancer <wyatharth@gmail.com>
2025-11-04 22:12:50 +0530

    updated .gitignore

---
+++ frontend/src/components/ui/input-with-label.tsx
@@ -0,0 +1,18 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import * as React from "react"
+
+export interface InputWithLabelProps extends React.ComponentProps<"input"> {
+  label: string;
+}
+
+export function InputWithLabel({ className, type, label, placeholder, ...props }: Readonly<InputWithLabelProps>) {
+  const inputId = label; 
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-3">
+      <Label htmlFor={inputId}>{label}</Label>
+      <Input type={type} id={inputId} placeholder={placeholder} {...props}/>
+    </div>
+  )
+}
